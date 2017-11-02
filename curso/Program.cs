38e906e1ae4bed020906071ed50a185d2675289b
Eder Bergmann Laurindo <eder.bergmann@gmail.com>
2017-11-02 10:51:35 -0200

    Acrescentado ator Eder

--- curso/Program.cs
+++ curso/Program.cs
@@ -16,6 +16,7 @@ namespace curso {
             artistas.Add(new Artista(102, "Chris Evans", 2500000.00));
             artistas.Add(new Artista(103, "Robert Downey Jr", 3000000.00));
             artistas.Add(new Artista(104, "Morgan Freeman", 4000000.00));
+            artistas.Add(new Artista(104, "Eder Laurindo", 4000000.00));
             artistas.Sort();
 
             while (opcao != 5) {
